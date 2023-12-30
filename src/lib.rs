f00d91b8cebdfb4c06032e18e0dc172f86ccea2f
u1roh <terada.yuichiro+github@gmail.com>
2023-12-30 22:13:55 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -21,76 +21,130 @@ impl Hoge for std::marker::PhantomData<usize> {
     }
 }
 
-trait Boundary<T> {
-    fn is_valid(&self, lower: T, upper: T) -> bool;
-    fn are_inclusive(&self) -> (bool, bool);
+pub trait Boundary: Copy + std::ops::Deref<Target = Self::Scalar> {
+    type Scalar: Copy + Ord;
+    fn val(&self) -> Self::Scalar;
+    fn is_inclusive(&self) -> bool;
+    fn contains(&self, t: Self::Scalar) -> bool;
+    fn includes(&self, other: &Self) -> bool;
+    fn intersection(&self, other: &Self) -> Self;
+    fn union(&self, other: &Self) -> Self;
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct ClosedInterval<T> {
-    pair: (T, T),
+pub trait LowerBoundary: Boundary {
+    type Complement: UpperBoundary<Scalar = Self::Scalar>;
+    fn complement(&self) -> Self::Complement;
 }
-impl<T> std::ops::Deref for ClosedInterval<T> {
-    type Target = (T, T);
+
+pub trait UpperBoundary: Boundary {
+    type Complement: LowerBoundary<Scalar = Self::Scalar>;
+    fn complement(&self) -> Self::Complement;
+}
+
+struct InclusiveLower<T>(T);
+impl<T> std::ops::Deref for InclusiveLower<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl<T: PartialOrd> InclusiveLower<T> {
+    pub fn contains(&self, t: T) -> bool {
+        self.0 <= t
+    }
+}
+
+pub type UnionSubtrahend<L, U> =
+    Interval<<U as UpperBoundary>::Complement, <L as LowerBoundary>::Complement>;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Interval<L, U> {
+    pair: (L, U),
+}
+impl<L, U> std::ops::Deref for Interval<L, U> {
+    type Target = (L, U);
     fn deref(&self) -> &Self::Target {
         &self.pair
     }
 }
-impl<T: Copy + Ord> ClosedInterval<T> {
-    pub fn new(lower: T, upper: T) -> Option<Self> {
-        (lower <= upper).then_some(Self {
+impl<L, U> Interval<L, U>
+where
+    L: LowerBoundary,
+    U: UpperBoundary<Scalar = L::Scalar>,
+{
+    pub fn new(lower: L, upper: U) -> Option<Self> {
+        (lower.contains(upper.val()) && upper.contains(lower.val())).then_some(Self {
             pair: (lower, upper),
         })
+        // (lower.is_less_or_eq(upper.val()) && upper.is_greater_or_eq(lower.val())).then_some(Self {
+        //     pair: (lower, upper),
+        // })
+        // (
+        //     *lower < *upper || (*lower == *upper && lower.is_inclusive() && upper.is_inclusive())
+        // )
+        // .then_some(Self {
+        //     pair: (lower, upper),
+        // })
     }
-    pub fn contains(&self, t: T) -> bool {
-        self.0 <= t && t <= self.1 // closed interval
+    pub fn inf(&self) -> L::Scalar {
+        self.0.val()
     }
+    pub fn sup(&self) -> U::Scalar {
+        self.1.val()
+    }
+    pub fn contains(&self, t: L::Scalar) -> bool {
+        self.0.contains(t) && self.1.contains(t)
+        // self.0.is_less_or_eq(t) && self.1.is_greater_or_eq(t)
+        // (*self.0 < t || (*self.0 == t && self.0.is_inclusive()))
+        //     && (t < *self.1 || (t == *self.1 && self.1.is_inclusive()))
+        // self.0 <= t && t <= self.1 // closed interval
+    }
+
     pub fn intersection(&self, other: &Self) -> Option<Self> {
-        Self::new(self.0.max(other.0), self.1.min(other.1))
+        Self::new(self.0.intersection(&other.0), self.1.intersection(&other.1))
     }
-    pub fn union(&self, other: &Self) -> (Self, Option<Self>) {
-        if self.1 < other.0 {
-            (*self, Some(*other))
-        } else if other.1 < self.0 {
-            (*other, Some(*self))
-        } else {
-            let pair = (self.0.min(other.0), self.1.max(other.1));
-            (Self { pair }, None)
+
+    pub fn union_interval(&self, other: &Self) -> Self {
+        Self {
+            pair: (self.0.union(&other.0), self.1.union(&other.1)),
         }
     }
+
+    pub fn union(&self, other: &Self) -> (Self, Option<UnionSubtrahend<L, U>>) {
+        let subtrahend = Interval::new(self.1.complement(), other.0.complement())
+            .or(Interval::new(other.1.complement(), self.0.complement()));
+        (self.union_interval(other), subtrahend)
+    }
+
     pub fn overlaps(&self, other: &Self) -> bool {
         self.intersection(other).is_some()
     }
+
     pub fn includes(&self, other: &Self) -> bool {
-        self.0 <= other.0 && other.1 <= self.1
-    }
-    pub fn signed_distance(&self, other: Self) -> T
-    where
-        T: std::ops::Sub,
-    {
+        self.0.includes(&other.0) && self.1.includes(&other.1)
     }
 }
 
-pub trait Interval<T>: Sized + std::ops::Deref<Target = (T, T)> {
-    fn lower(&self) -> T;
-    fn upper(&self) -> T;
-    fn is_inclusive(&self) -> (bool, bool);
-    fn measure(&self) -> T;
-    fn contains(&self, t: T) -> bool;
-    fn intersection(&self, other: &Self) -> Option<Self>;
-    fn union(&self, other: &Self) -> (Self, Option<Self>);
-}
+// pub trait Interval<T>: Sized + std::ops::Deref<Target = (T, T)> {
+//     fn lower(&self) -> T;
+//     fn upper(&self) -> T;
+//     fn is_inclusive(&self) -> (bool, bool);
+//     fn measure(&self) -> T;
+//     fn contains(&self, t: T) -> bool;
+//     fn intersection(&self, other: &Self) -> Option<Self>;
+//     fn union(&self, other: &Self) -> (Self, Option<Self>);
+// }
 
-pub trait IntervalSet<T>: std::ops::Deref<Target = [Self::Interval]> {
-    type Interval: Interval<T>;
-    type Complement: IntervalSet<T>;
-    type Difference: IntervalSet<T>;
-    fn intersection(&self, other: &Self) -> Self;
-    fn union(&self, other: &Self) -> Self;
-    fn complement(&self) -> Self::Complement;
-    fn measure(&self) -> T;
-    fn overlaps(&self, other: &Self) -> bool;
-}
+// pub trait IntervalSet<T>: std::ops::Deref<Target = [Self::Interval]> {
+//     type Interval: Interval<T>;
+//     type Complement: IntervalSet<T>;
+//     type Difference: IntervalSet<T>;
+//     fn intersection(&self, other: &Self) -> Self;
+//     fn union(&self, other: &Self) -> Self;
+//     fn complement(&self) -> Self::Complement;
+//     fn measure(&self) -> T;
+//     fn overlaps(&self, other: &Self) -> bool;
+// }
 
 #[cfg(test)]
 mod tests {
